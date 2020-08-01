a8adf8fe7af609c1b9fb998294b851c69a5e1021
Martin Svensson <msvens@gmail.com>
2020-08-01 14:31:29 +0200

    Did a major overhaul of the database layer. Moved to a separate package and split the implementation up in multiple files. Fixed bugs with not closing DB Rows

--- internal/model/upgrade.go
+++ internal/model/upgrade.go
@@ -1,13 +1,11 @@
-package service
+package model
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func (dbs *DbService) UpgradeDb() error {
+func UpgradeDb(db *DB) error {
 
 	//Check for version 1:
-	rows, err := dbs.Db.Query("SELECT * from photos limit 1")
+	rows, err := db.Query("SELECT * from photos limit 1")
 	if err != nil {
 		return err
 	}
@@ -24,17 +22,12 @@ func (dbs *DbService) UpgradeDb() error {
 	}
 
 	if !hasPrivate {
-		return upgradeToV4FromV1(dbs)
-	}
-
-	//Check for version 2:
-	if _, err := dbs.Db.Query(getAlbumsStmt); err != nil {
-		return upgradeToV3FromV2(dbs)
+		return upgradeToV4FromV1(db)
 	}
 
 	//Check for version 3:
-	if _, err := dbs.Db.Query("SELECT * from albumphoto"); err != nil {
-		return upgradeToV4FromV3(dbs)
+	if _, err := db.Query("SELECT * from albumphoto"); err != nil {
+		return upgradeToV4FromV3(db)
 	}
 
 	fmt.Println("Databes looks to be up to date")
@@ -44,18 +37,18 @@ func (dbs *DbService) UpgradeDb() error {
 
 }
 
-func upgradeToV4FromV3(dbs *DbService) error {
+func upgradeToV4FromV3(db *DB) error {
 	photoAlbumQ := "SELECT driveId,album from photos WHERE album != ''"
 	dropAlbumColumn := "ALTER TABLE photos DROP COLUMN album"
 
 	fmt.Println("Upgrading mphotos db from Version 3 to Version 4")
 	fmt.Println("Adding albumphoto table")
-	if _, err := dbs.Db.Exec(createAlbumPhotoTable); err != nil {
+	if err := db.CreateAlbumPhotoStore(); err != nil {
 		return err
 	}
 	//move album/photo information to albumphoto
 	fmt.Println("Adding album-photo mappings to albumphoto")
-	if rows, err := dbs.Db.Query(photoAlbumQ); err != nil {
+	if rows, err := db.Query(photoAlbumQ); err != nil {
 		return err
 	} else {
 		for rows.Next() {
@@ -63,49 +56,18 @@ func upgradeToV4FromV3(dbs *DbService) error {
 			var a string
 			rows.Scan(&id, &a)
 			albums := trimAndSplit(a)
-			dbs.UpdatePhotoAlbum(albums, id)
+			db.UpdatePhotoAlbums(albums, id)
 		}
 	}
 	//drop album column
 	fmt.Println("Drop album column from photos")
-	if _, err := dbs.Db.Exec(dropAlbumColumn); err != nil {
+	if _, err := db.Exec(dropAlbumColumn); err != nil {
 		return err
 	}
 	return nil
 }
 
-func upgradeToV3FromV2(dbs *DbService) error {
-	fmt.Println("Upgrading mphotos db from Version 2 to Version 3")
-	fmt.Println("Adding Album Table")
-	if _, err := dbs.Db.Exec(createAlbumTable); err != nil {
-		return err
-	}
-
-	fmt.Println("Adding Albums from photos")
-	if rows, err := dbs.Db.Query(distinctAlbumsStmt); err != nil {
-		return err
-	} else {
-		for rows.Next() {
-			var a string
-			rows.Scan(&a)
-			albums := trimAndSplit(a)
-			for _, album := range albums {
-				if album != "" && !dbs.ContainsAlbum(album) {
-					fmt.Println("adding album: ", album)
-					err := dbs.AddAlbum(&Album{Name: album})
-					if err != nil {
-						return err
-					}
-				}
-			}
-		}
-	}
-	return nil
-}
-
-//TODO: Update to V4 (and not V3)
-
-func upgradeToV4FromV1(dbs *DbService) error {
+func upgradeToV4FromV1(db *DB) error {
 	addColumnsPhotosV2 := `
 ALTER TABLE photos
 	ADD COLUMN private BOOLEAN,
@@ -121,30 +83,30 @@ ALTER TABLE photos
 
 	fmt.Println("Upgrading mphotos db from Version 1 to Version 4")
 	fmt.Println("Adding columns to photos")
-	_, err := dbs.Db.Exec(addColumnsPhotosV2)
+	_, err := db.Exec(addColumnsPhotosV2)
 	if err != nil {
 		return err
 	}
 	fmt.Println("Set column values")
-	_, err = dbs.Db.Exec(setColumnsPhotosV2)
+	_, err = db.Exec(setColumnsPhotosV2)
 	if err != nil {
 		return err
 	}
 	fmt.Println("Upgrading mphotos db to Version 2")
 	fmt.Println("Sec column constraint not null")
-	_, err = dbs.Db.Exec(setConstraintsPhotosV2)
+	_, err = db.Exec(setConstraintsPhotosV2)
 
 	if err != nil {
 		return err
 	}
 
 	fmt.Println("Create Album Table")
-	_, err = dbs.Db.Exec(createAlbumTable)
+	err = db.CreateAlbumStore()
 	if err != nil {
 		return err
 	}
 
 	fmt.Println("Create AlbumPhoto Table")
-	_, err = dbs.Db.Exec(createAlbumPhotoTable)
+	err = db.CreateAlbumPhotoStore()
 	return err
 }
